58d9a5ef9557becedfb4e38ee8032554eb1d3d17
Alfonso 7l <alfonso@7lfreight.com>
2020-01-21 11:11:59 -0300

    remove big  file

--- streams/big-file.js
+++ streams/big-file.js
@@ -4,6 +4,9 @@ const file = fs.createWriteStream('./big');
 //  events: drain. Se dispara cuando emite datos, finish. Se dispara cuando termina de emitir, error. Se dispara cuando hay un error
 //  functions: write, end
 //
+// git filter-branch --tree-filter 'rm -rf path/to/your/file' HEAD
+
+
 for (let index = 0; index < 1e6; index++) {
     file.write('Estaba la Pájara Pinta sentadita en el verde limón. Con el pico cortaba la rama, con la rama cortaba la flor.');    
 }
