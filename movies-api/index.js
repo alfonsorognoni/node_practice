4944471c6d3c7e29a904baeceac312b3df6caf43
Alfonso 7l <alfonso@7lfreight.com>
2020-01-22 09:54:00 -0300

    leap year

--- movies-api/index.js
+++ movies-api/index.js
@@ -10,6 +10,15 @@ app.get('/json', function (req, res) {
     res.json({ hello: 'world' });
 });
 
+app.get('/year/:year?', function (req, res) {
+    if (!req.params.year) {
+        res.status(404).send("Sorry, send a year please <br/>eg: year/2020");
+    }
+    const year = req.params.year;
+    const checkFebruary = new Date(year, 2, 0).getDate() > 28 ? 'It\'s a leap year' : 'It\'s not a leap year';
+    res.send(checkFebruary);
+});
+
 app.listen(config.port, function () {
     console.log(`Listening http://localhost:${config.port}`);
 });
